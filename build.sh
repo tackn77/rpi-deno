8a98e4f21583a3b83360796396e8f266ee68542b
Takumi Ito <tackn1977@gmail.com>
2023-02-27 01:08:33 +0900

    定期実行ビルド対応

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-echo DENO_VERSION=`curl --silent https://api.github.com/repos/denoland/deno/releases/latest | jq .tag_name` > .env
+tagname=`curl --silent https://api.github.com/repos/denoland/deno/releases/latest | jq --raw-output .tag_name`
+envname=DENO_VERSION=${tagname}
 
-docker build .--no-cache --force-rm --cpuset-cpus 0-1
+if [ -f .env ]; then
+    check=`cat .env`
+else
+    check="Not File"
+fi
+
+if [[ $envname == $check ]]; then
+  echo "Build Exists"
+else
+  echo ${envname} > .env
+  docker build . --no-cache --force-rm --tag tackn/deno:${tagname} --cpuset-cpus 0-1
+fi
